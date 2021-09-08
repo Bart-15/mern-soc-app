609e0ebe81a600fd335cb9cca722d41b11bcc5e9
Bart-15 <brat.tabusao@gmail.com>
2021-09-08 17:32:45 +0800

    Add Post and Profile routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,94 @@
 const express = require('express');
-
+const passport = require('passport');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+// Load validation for post request
+const validatePostInput = require('../../validation/post');
 const router = express.Router();
 
+// Create Post
+// Private
+router.post('/api/posts', passport.authenticate('jwt', {session:false}), async (req, res) => {
+   
 
-router.get('/post/test', async (req, res) => {
-    res.json({msg:"Test message"})
+    const {errors, isValid} = validatePostInput(req.body)
+
+    if(!isValid) {
+        return res.status(400).json(errors)
+    }
+
+    const newPost = {
+        user: req.user._id, 
+        text: req.body.text,
+        avatar: req.body.avatar,    
+        name: req.body.name, 
+    }
+
+    try{
+       const post =  await new Post(newPost).save();
+       res.status(200).json(post)
+    }catch(e) {
+        res.status(400).send();
+    }
+
+})
+
+
+// Fetch All Post
+// Public
+router.get('/api/posts', async (req, res) => {
+    const post = await Post.find({});
+    const errors = {}
+    try{
+        if(!post){
+            errors.nopost = "No post available right now."
+            return res.status(404).json(errors)
+        }
+
+
+        res.status(200).json(post)
+    }catch(e) {
+        res.status(500).send(); 
+    }
+})
+
+
+// Fetch post by ID
+// Public
+router.get('/api/posts/:id', async (req, res) => {
+    const post = await Post.findById({_id: req.params.id});
+    const errors = {};
+
+    try{
+        if(!post){
+            errors.nopost = "Post not found";
+            return res.status(404).send(errors);
+        }
+
+        res.status(200).json(post)
+    }catch(e){
+        res.status(400).send();
+    }
+})
+
+// delete post by id
+// Private Route
+
+router.delete('/api/post/:id', passport.authenticate('jwt', {session:false}), async (req, res) =>{
+    const profile = await Profile.findOne({user: req.user._id});
+    const post = await Post.findById({_id :req.params.id})
+    
+    try{
+            if(profile.user.toString() !== req.user._id){
+                return res.status(401).json({unauthorized:"Unathorized"})
+            }
+            await post.remove()
+            res.json({success: true})
+        
+
+    }catch(err){
+        res.status(404).send();
+    }
 })
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-// validateProfileInput
+// validation config
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
+
+
+
 
 
 // Profile
@@ -43,7 +48,7 @@ router.post('/api/profile', passport.authenticate('jwt', {session:false}), async
     const {errors, isValid} = validateProfileInput(req.body);
 
     if(!isValid){
-        return res.status(401).json(errors);
+        return res.status(400).json(errors);
     }
     
     const profileFields = {};
@@ -73,7 +78,6 @@ router.post('/api/profile', passport.authenticate('jwt', {session:false}), async
 
     // find the profile then update if it exist and if not then create;
     const user = await Profile.findOne({user: req.user._id});
-
     try{
         if(user){
            const profile = await Profile.findOneAndUpdate({user: req.user._id}, profileFields, {new: true})
@@ -88,12 +92,193 @@ router.post('/api/profile', passport.authenticate('jwt', {session:false}), async
     }catch(err){
         console.log(err);
     }
-    
-
-  
 
 })
 
 
+// get /api/profile/handle/:handle
+// get profile by Handle
+router.get('/api/profile/handle/:handle', async (req, res) => {
+    const errors = {}
+    const profile = await Profile.findOne({handle: req.params.handle}).populate('user', ['name', 'avatar'])
 
+    try{
+        if(!profile){
+            errors.noprofile = "Profile not found"
+            return res.status(404).json(errors)
+        }
+
+        res.status(200).json(profile)
+    }catch (err){
+        res.status(401).send(err)
+    }
+});
+
+
+// GET profile by ._id
+// public route
+router.get('/api/user/:user_id', async (req, res) => {
+    const errors = {}
+
+    const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar'])
+    try{
+        if(!profile){
+            errors.noprofile = "There is no profile for this user";
+            return res.status(404).send(errors);   
+        }
+
+        res.status(200).json(profile)
+    }catch(err){
+        res.status(404).send({errors:"Theres no profile for this"})
+    }
+})
+
+// get profile all users
+// public route
+router.get('/api/all', async (req, res) => {
+    const errors = {}
+    const profile = await Profile.find({}).populate('user', ['name', 'avatar']);
+
+
+    try{
+        if(!profile){
+            errors.noprofile = "There's no profile this time"
+            return res.status(404).send(errors)
+        }
+
+
+        res.status(200).json(profile)
+    }catch(err){
+        res.ststatus(404).send({errors:"There's no profile this time"})
+    }
+})
+
+
+// @route POST api/profile/experience
+router.post('/api/profile/experience', passport.authenticate('jwt', {session:false}), async (req, res) => {
+    const {errors, isValid} = validateExperienceInput(req.body)
+
+    if(!isValid) {
+        return res.status(400).json(errors)
+    }
+
+
+    const newExperience = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+    };
+
+    try{
+        const profile = await Profile.findOne({user: req.user._id});
+        profile.experience.unshift(newExperience)
+        await profile.save();
+        res.json(profile);
+    }catch(err){
+        res.status(400).send()
+    }
+
+});
+
+
+
+
+// @route POST api/profile/education
+// add education
+router.post('/api/profile/education',  passport.authenticate('jwt', {session:false}), async (req, res) => {
+    const {errors, isValid} = validateEducationInput(req.body)
+
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
+    const newEduc = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        description: req.body.description,
+    }
+
+    try{
+        const profile = await Profile.findOne({user: req.user._id});
+        if(!profile){
+            return res.status(404).json({noprofile:"No profile found"})
+        }
+
+        profile.education.unshift(newEduc);
+        await profile.save();
+        res.status(200).json(profile)
+
+    }catch(e){
+        res.status(400).send()
+    }
+})
+
+
+
+// delete experience
+// /api/experience/experience/:epx_id
+router.delete('/api/profile/experience/:exp_id', passport.authenticate('jwt', {session:false}), async (req, res) => {
+ 
+    // You can use the filter method to delete the experience
+    //  const removeExp =  profile.experience.filter(item => item._id !== req.params.exp_id)
+    //  profile.experience.splice(removeExp, 1)
+
+    const profile = await Profile.findOne({user: req.user._id});
+
+    try{
+    if(!profile){
+        return res.status(404).json()
+    }
+
+    // Get remove the object
+    const removeIndex = profile.experience.map(item => item._id).indexOf(req.params.exp_id);
+    
+    //Splice the array
+    profile.experience.splice(removeIndex, 1)
+
+    // then save
+     await profile.save()
+     res.status(200).json(profile)
+    
+    }catch(err){
+        res.status(404).send()
+    }
+});
+
+
+// delete education 
+// private
+// /api/profile/education/:edc_id
+router.delete('/api/profile/education/:edu_id', passport.authenticate('jwt', {session:false}), async (req, res) => {
+    const profile = await Profile.findOne({user: req.user._id});
+
+    try{
+        if(!profile){
+            return res.status(404).send();
+        }
+    // filter the profile education profile
+    const removeEducation = profile.education.filter(item => item._id !== req.params.edu_id);
+    profile.education.splice(removeEducation, 1);
+    await profile.save();
+    res.status(200).json(profile);
+    }catch (err){
+        res.status(404).send()
+    }
+
+})
+
+
+// delete user and profileFields
+router.delete('/api/profile', passport.authenticate('jwt', {session:false}), async (req, res) => {
+     await Profile.findOneAndRemove({user: req.user._id});
+     await User.findOneAndRemove({_id: req.user._id});
+     res.json({success: true})
+})
 module.exports = router;
