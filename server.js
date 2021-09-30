ca4b07f17b013c3b9585b18362ddd703b1bfc14f
Bart-15 <brat.tabusao@gmail.com>
2021-09-30 16:28:34 +0800

    Fixing bug

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use(profile)
 // server static assets if in production
 if(process.env.NODE_ENV == 'production') {
     // Set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static('./client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
     })
 }
 
